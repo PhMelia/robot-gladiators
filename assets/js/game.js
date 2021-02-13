330bd5d0012b037e620f9c6437238a8c9aded836
usmelphi <Phillip.Melia.JW@gmail.comgit config --global user.emailgit config --global user.email Phillip.Melia.JW@gmail.com>
2021-02-13 06:08:42 -0800

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,9 +74,16 @@ var fight = function(enemyName) {
  
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames [i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+  if (playerHealth > 0) {
+    window.alert("Welcome to Robot Gladiators!" + (i + 1));
+  }  
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
+  var pickedEnemyName = enemyNames [i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
 
 // Game States
